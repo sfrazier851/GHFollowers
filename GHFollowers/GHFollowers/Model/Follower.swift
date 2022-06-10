e61519d3003418b2e6018e59b94b15da820f99fd
sfrazier851 <spencer851@revature.net>
2022-06-10 17:15:14 -0400

    add model objects (follower and user)

---
+++ GHFollowers/GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by admin on 6/10/22.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
