38c8ed01113fa58b4820eb3443efcc9667e0c013
sfrazier851 <spencer851@revature.net>
2022-06-11 13:27:03 -0400

    add ErrorMessage enum and refactor getFollowers call

---
+++ GHFollowers/GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by admin on 6/11/22.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUsername = "This username created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connetion."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
