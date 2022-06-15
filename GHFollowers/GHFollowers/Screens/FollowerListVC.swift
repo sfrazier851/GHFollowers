e0b58c3452160bff7a0d748023f1504cab6d7916
sfrazier851 <spencer851@revature.net>
2022-06-15 00:23:47 -0400

    add loading spinner view

--- GHFollowers/GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/GHFollowers/Screens/FollowerListVC.swift
@@ -49,8 +49,11 @@ class FollowerListVC: UIViewController {
     }
     
     private func getFollowers(username: String, page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
+            
+            self.dismissLoadingView()
             
             switch result {
             case .success(let followers):
