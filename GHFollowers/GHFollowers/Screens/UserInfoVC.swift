744c4225cbb1a849dfc5e4421744d4c5fa317011
sfrazier851 <spencer851@revature.net>
2022-07-05 12:53:18 -0400

    separate out IntemInfoVCDelegate (GFFollowerItemVCDelegate & GFRepoItemVCDelegate) and init(user:,delegate:) for both

--- GHFollowers/GHFollowers/Screens/UserInfoVC.swift
+++ GHFollowers/GHFollowers/Screens/UserInfoVC.swift
@@ -51,15 +51,9 @@ class UserInfoVC: GFDataLoadingVC {
     }
     
     private func configureUIElements(with user: User) {
-        let repoItemVC = GFRepoItemVC(user: user)
-        repoItemVC.delegate = self
-        
-        let followerItemVC = GFFollowerItemVC(user: user)
-        followerItemVC.delegate = self
-        
         self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
-        self.add(childVC: repoItemVC, to: self.itemViewOne)
-        self.add(childVC: followerItemVC, to: self.itemViewTwo)
+        self.add(childVC: GFRepoItemVC(user: user, delegate: self), to: self.itemViewOne)
+        self.add(childVC: GFFollowerItemVC(user: user, delegate: self), to: self.itemViewTwo)
         self.dateLabel.text = "GitHub since \(user.createdAt.convertToMonthYearFormat())" //"\(user.createdAt.convertToDisplayFormat())"
     }
     
@@ -106,14 +100,16 @@ class UserInfoVC: GFDataLoadingVC {
 }
 
 
-extension UserInfoVC: ItemInfoVCDelegate {
+extension UserInfoVC: GFRepoItemVCDelegate {
     func didTapGitHubProfile(for user: User) {
         guard let url = URL(string: user.htmlUrl) else { presentGFAlertOnMainThread(title: "Invalid URL.", message: "The url attached to this user is invalid", buttonTitle: "Ok")
             return
         }
         presentSafariVC(with: url)
     }
-    
+}
+
+extension UserInfoVC: GFFollowerItemVCDelegate {
     func didTapGetFollowers(for user: User) {
         guard user.followers != 0 else {
             presentGFAlertOnMainThread(title: "No Followers", message: "This user has no followers. What a shame 😔.", buttonTitle: "So Sad")
